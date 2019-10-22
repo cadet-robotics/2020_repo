f88df5f73cdf185d67228e4de4eb44c40fe81eeb
21oavery <21oavery@ga.hiltoncsd.net>
2019-10-22 17:39:04 -0400

    Removed old code

--- src/main/java/frc/robot/JNI.java
+++ src/main/java/frc/robot/JNI.java
@@ -1,11 +1,4 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Filesystem;
-
 public class JNI {
-    static {
-        System.loadLibrary(Filesystem.getDeployDirectory().toString() + "/rustpart.so");
-    }
-
-    public static native int test(int i);
 }
