58874e09b5def3e1548fc611e797c1383929542e
21oavery <21oavery@ga.hiltoncsd.net>
2020-02-20 21:20:33 -0500

    Switched the flywheel encoders over to CANEncoders

--- src/main/java/frc/robot/io/Sensors.java
+++ src/main/java/frc/robot/io/Sensors.java
@@ -1,15 +1,19 @@
 package frc.robot.io;
 
+import com.revrobotics.CANEncoder;
+import com.revrobotics.EncoderType;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.interfaces.Gyro;
 import frc6868.config.api.Config;
 
 public class Sensors {
-    public static Encoder topFlyEncoder,
-                          bottomFlyEncoder,
-                          driveEncoderLeft,
+    public static CANEncoder topFlyEncoder,
+                             bottomFlyEncoder;
+
+    public static Encoder driveEncoderLeft,
                           driveEncoderRight;
+
     public static Gyro gyro;
     
     // Proximity sensors for the magazine
@@ -27,8 +31,8 @@ public class Sensors {
         bottomBallSensor = new DigitalInput(dioSensors.getIntValue("bottom ball sensor"));
 
         // Encoders
-        topFlyEncoder = new Encoder(dioSensors.getIntValue("top shooter encoder 1"), dioSensors.getIntValue("top shooter encoder 2"));
-        bottomFlyEncoder = new Encoder(dioSensors.getIntValue("bottom shooter encoder 1"), dioSensors.getIntValue("bottom shooter encoder 2"));
+        topFlyEncoder = new CANEncoder(Motors.topFly, EncoderType.kQuadrature, 8192);
+        bottomFlyEncoder = new CANEncoder(Motors.bottomFly, EncoderType.kQuadrature, 8192);
         driveEncoderLeft = new Encoder(dioSensors.getIntValue("left drive encoder 1"), dioSensors.getIntValue("left drive encoder 2"));
         driveEncoderRight = new Encoder(dioSensors.getIntValue("right drive encoder 1"), dioSensors.getIntValue("right drive encoder 2"));
 
@@ -37,11 +41,5 @@ public class Sensors {
         final double DIST_DRIVE = 6.0 * .0254 * Math.PI / 1024.0;
         driveEncoderLeft.setDistancePerPulse(DIST_DRIVE);
         driveEncoderRight.setDistancePerPulse(DIST_DRIVE);
-
-        // Sets up shooter encoders to report rate as rpm
-        // TODO: Check gear ratio between encoder and wheels
-        final double DIST_FLY = 1.0 / 1024.0 / 60.0;
-        topFlyEncoder.setDistancePerPulse(DIST_FLY);
-        bottomFlyEncoder.setDistancePerPulse(DIST_FLY);
     }
 }
