df0334783866a059a0463d7f51ad8059b21a8ca3
Matthew <minerminesmc@gmail.com>
2020-02-24 18:07:41 -0500

    More efficient and added auto-intake turn off

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -16,6 +16,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.commands.SkitterCommand;
@@ -191,6 +192,9 @@ public class Robot extends TimedRobot {
         CommandScheduler.getInstance().run();
         System.out.println("SPEED TOP: " + Sensors.bottomFlyEncoder.getVelocity());
         System.out.println("SPEED BOTTOM: " + Sensors.bottomFlyEncoder.getVelocity());
+
+        //Debug and Smartdashboard
+        SmartDashboard.putBoolean("Auto Ball Intake", pickupSubsystem.getAutoIntakeEnabled());
     }
 
     /**
--- src/main/java/frc/robot/commands/magazine/IntakeNewBallCommand.java
+++ src/main/java/frc/robot/commands/magazine/IntakeNewBallCommand.java
@@ -1,8 +1,6 @@
 package frc.robot.commands.magazine;
 
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.io.Motors;
 import frc.robot.subsystems.PickupSubsystem;
 
 /**
