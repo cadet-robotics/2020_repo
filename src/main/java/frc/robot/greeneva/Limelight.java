0fa3f8be798a74634c0e83871b5f9073eeddb825
21oavery <21oavery@ga.hiltoncsd.net>
2020-02-24 17:53:11 -0500

    Adds ability to switch cam mode

--- src/main/java/frc/robot/greeneva/Limelight.java
+++ src/main/java/frc/robot/greeneva/Limelight.java
@@ -10,12 +10,14 @@ public class Limelight {
     private NetworkTableEntry hasTarget;
     private NetworkTableEntry hAng;
     private NetworkTableEntry vAng;
+    private NetworkTableEntry camMode;
 
     public Limelight(NetworkTableInstance ntI) {
         NetworkTable lim = ntI.getTable("limelight");
         hasTarget = lim.getEntry("tv");
         hAng = lim.getEntry("tx");
         vAng = lim.getEntry("ty");
+        camMode = lim.getEntry("camMode");
     }
 
     public boolean hasTarget() {
@@ -42,5 +44,21 @@ public class Limelight {
     // Limelight recommends this, and it does seem like it would work well
     public double getDistance() {
         return (TARGET_HEIGHT - LIMELIGHT_HEIGHT) / Math.tan(getVAngleRad() + LIMELIGHT_ANGLE);
+    }
+
+    public enum CamMode {
+        Vision,
+        Driver
+    }
+
+    public void setCamMode(CamMode mode) {
+        switch (mode) {
+            case Vision:
+                camMode.setNumber(0);
+                break;
+            case Driver:
+                camMode.setNumber(1);
+                break;
+        }
     }
 }
