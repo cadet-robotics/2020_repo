a6d02422db59992115165089c029e575785308dc
Matthew <minerminesmc@gmail.com>
2020-02-24 17:36:35 -0500

    Completed all commands and added it to the pickup subsystem
    And it works probably

--- src/main/java/frc/robot/commands/magazine/IntakeNewBallCommand.java
+++ src/main/java/frc/robot/commands/magazine/IntakeNewBallCommand.java
@@ -2,24 +2,41 @@ package frc.robot.commands.magazine;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.io.Motors;
 import frc.robot.subsystems.PickupSubsystem;
 
 /**
  * The overlord of all mag commands. He manifests power. He also takes in new balls
  * and stores them in the shooter. Either way: power.
  *
+ * B1 - The position of the ball in the intake. Ready to be picked up
+ * B2 - The position of the ball when entering the magazine.
+ * B3 - Some ball above B2. Must be translated upwards.
+ *
  * @author Matt Robinson
  */
 public class IntakeNewBallCommand extends SequentialCommandGroup {
+    PickupSubsystem pickupSubsystem;
 
     /**
      * Constructor. Executes B1 -> B2 & B2 -> B3
      */
     public IntakeNewBallCommand(PickupSubsystem pickupSubsystemIn) {
         super(
-                new InstantCommand(() -> new BallToMagCommand(pickupSubsystemIn)),
-                new InstantCommand(() -> new TranslateMagCommand(pickupSubsystemIn))
+                new BallToMagCommand(pickupSubsystemIn),
+                new TranslateMagCommand(pickupSubsystemIn)
         );
+        pickupSubsystem = pickupSubsystemIn;
     }
 
+    /**
+     * Turns off motors and such
+     *
+     * @param interrupt Unsure what this does but its important
+     */
+    public void end(boolean interrupt) {
+        super.end(interrupt);
+        pickupSubsystem.setIntakeSpeed(0);
+        pickupSubsystem.setMagazineSpeed(0);
+    }
 }
