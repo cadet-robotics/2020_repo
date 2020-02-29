1f837ab1de8b8c33c946a01e288cb0d59c7f652a
21oavery <21oavery@ga.hiltoncsd.net>
2020-02-29 10:19:25 -0500

    Added PID to LimelightFacer

--- src/main/java/frc/robot/commands/LimelightFacer.java
+++ src/main/java/frc/robot/commands/LimelightFacer.java
@@ -1,5 +1,6 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.greeneva.Limelight;
@@ -8,12 +9,14 @@ import frc.robot.subsystems.DriveSubsystem;
 public class LimelightFacer extends CommandBase {
     private Limelight lime;
     private DriveSubsystem driveSubsystem;
+    private PIDController pid;
 
     public LimelightFacer(Limelight limeIn, DriveSubsystem driveSubsystemIn) {
         super();
         lime = limeIn;
         driveSubsystem = driveSubsystemIn;
         addRequirements(driveSubsystem);
+        pid = new PIDController(1 / 54.0, 0, 0);
     }
 
     private static final SimpleMotorFeedforward feed = new SimpleMotorFeedforward(0.2, 0.8);
@@ -22,15 +25,14 @@ public class LimelightFacer extends CommandBase {
 
     @Override
     public void execute() {
-        // CCW rotation speed
-        double speed = lime.hasTarget() ? lime.getHAngle() : -27;
-        if (speed > 1) {
-            speed /= 27;
-            speed *= 0.75;
-            speed = feed.calculate(speed);
-            driveSubsystem.getDriveBase().tankDrive(-speed, speed);
-        } else {
+        // CW rotation speed
+        double ang = lime.hasTarget() ? lime.getHAngle() : -27;
+        double speed = pid.calculate(ang);
+        if (pid.atSetpoint()) {
             isDone = true;
+        } else {
+            speed = feed.calculate(speed);
+            driveSubsystem.getDriveBase().tankDrive(speed, -speed);
         }
     }
 
