3e2c258c3a5c2471f56648e1bcd589d37f12092d
21oavery <21oavery@ga.hiltoncsd.net>
2020-03-04 17:09:59 -0500

    Quick adjustment for autonomous to use ShooterCommand

--- src/main/java/frc/robot/commands/AutoCommand.java
+++ src/main/java/frc/robot/commands/AutoCommand.java
@@ -3,10 +3,11 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.greeneva.Limelight;
 import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.PickupSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 
 public class AutoCommand extends SequentialCommandGroup {
-    public AutoCommand(Limelight lime, DriveSubsystem drive, ShooterSubsystem shooter) {
-        super(new SkitterCommand(drive), new RotateToLightCommand(lime, drive), new SetShooterSpeedCommand(shooter, 2000, 5));
+    public AutoCommand(Limelight lime, DriveSubsystem drive, ShooterSubsystem shooter, PickupSubsystem pick) {
+        super(new SkitterCommand(drive), new RotateToLightCommand(lime, drive), new ShooterCommand(pick, false));
     }
 }
