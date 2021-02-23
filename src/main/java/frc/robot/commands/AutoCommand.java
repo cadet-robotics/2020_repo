95adbb1e2ce42a138c751b80fe2f446ce52684f1
powerboat9 <21oavery@ga.hiltoncsd.net>
2021-02-22 19:03:09 -0500

    Did some more work on autonomous

--- src/main/java/frc/robot/commands/AutoCommand.java
+++ src/main/java/frc/robot/commands/AutoCommand.java
@@ -1,18 +1,28 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.greeneva.Limelight;
 import frc.robot.subsystems.DriveSubsystem;
-import frc.robot.subsystems.PickupSubsystem;
-import frc.robot.subsystems.ShooterSubsystem;
-
-import static frc.robot.trajectory.TrajectoryManager.BASE_TRAJECTORY;
-import static frc.robot.trajectory.TrajectoryManager.INITIAL_POS;
+import frc.robot.trajectory.TrajectoryManager;
 
 public class AutoCommand extends SequentialCommandGroup {
-    public AutoCommand(Limelight lime, DriveSubsystem drive, ShooterSubsystem shooter, PickupSubsystem pick) {
-        super(drive.trajectoryCommandBuilder(BASE_TRAJECTORY, INITIAL_POS));
+    public AutoCommand(DriveSubsystem drive, String type) {
+        Trajectory[] tls;
+        switch (type) {
+            case "BARREL":
+                tls = new Trajectory[] {TrajectoryManager.BARREL_TRAJECTORY};
+                break;
+            case "SLALOM":
+                tls = new Trajectory[] {TrajectoryManager.SLALOM_TRAJECTORY};
+                break;
+            case "BOUNCE":
+                tls = TrajectoryManager.BOUNCE_TRAJECTORY_LIST;
+                break;
+            default:
+                throw new RuntimeException("Invalid autonomous");
+        }
+        for (Trajectory t : tls) {
+            addCommands(drive.trajectoryCommandBuilder(t, t.getInitialPose()));
+        }
     }
 }
