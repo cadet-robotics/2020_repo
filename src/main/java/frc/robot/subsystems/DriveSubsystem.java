7b4fe64cc85575d42f4d44c24e75f53e70ef506a
21oavery <21oavery@ga.hiltoncsd.net>
2021-02-27 11:55:02 -0500

    Saving work

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,11 +1,6 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANEncoder;
-
-import com.revrobotics.EncoderType;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.controller.PIDController;
@@ -20,18 +15,19 @@ import edu.wpi.first.wpilibj.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
-import edu.wpi.first.wpilibj.util.Units;
-import edu.wpi.first.wpilibj2.command.*;
-import frc.robot.commands.TeleDriveCommand;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.io.Motors;
 import frc.robot.io.Sensors;
 
 import static frc.robot.Constants.MAX_SPEED;
 
 public class DriveSubsystem extends SubsystemBase {
-    private static final SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(3 / MAX_SPEED, 12 / MAX_SPEED);
+    private static final SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(3 / MAX_SPEED, 16 / MAX_SPEED);
 
-    private static final DifferentialDriveKinematics kin = new DifferentialDriveKinematics(Units.inchesToMeters(28));
+    private static final DifferentialDriveKinematics kin = new DifferentialDriveKinematics(0.64);
 
     private DifferentialDrive driveBase;
 
@@ -94,7 +90,7 @@ public class DriveSubsystem extends SubsystemBase {
                     //System.out.println(":> " + o);
                     return o;
                 },
-                new RamseteController(),
+                new RamseteController(2.0, 0.7),
                 feedforward,
                 kin,
                 () -> new DifferentialDriveWheelSpeeds(leftEncoder.getVelocity(), rightEncoder.getVelocity()),
